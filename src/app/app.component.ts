47261ce3126379e28df813b70c636f5240ced2ce
bertman12 <eddydaniel94@gmail.com>
2021-06-18 16:52:27 -0700

    initial upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-assignment';
+
+  name: string = '';
+  age: number = null;
+  city: string = '';
+  userNumber: number = null;
+  updateUser: boolean = false;
+  userSet:User[]  = [];
+  selectedUser: User = null;
+
+  onAddUser(){
+    this.userSet.push(new User(this.name,this.age,this.city));
+  }
+
+  onRemoveUserRequest(user: User){
+    this.userSet.splice(this.userSet.indexOf(user), 1);
+  }
+
+  onUpdateUserRequest(user: User){
+    this.updateUser = true;
+    this.selectedUser = user;
+  }
+
+  onFinishUpdateUser(){
+    this.updateUser = false;
+  }
+
 }
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { User } from '../user.model';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+
+export class ListComponent implements OnInit {
+
+    @Input() currentUser: User = null;
+    @Output() userRemoveRequest = new EventEmitter<User>();
+    @Output() userUpdateRequest = new EventEmitter<User>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRemoveUser(){
+    this.userRemoveRequest.emit(this.currentUser);
+  }
+
+  onUpdateUser(){
+    this.userUpdateRequest.emit(this.currentUser);
+  }
+}
